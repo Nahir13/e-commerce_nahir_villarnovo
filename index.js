db0941a8cae74f9e0a232dd7425f709bf4f23cad
Nahir <152107911+Nahir13@users.noreply.github.com>
2024-08-29 21:02:19 -0300

    add const

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Holaaaa");
+const h5 = document.querySelector("h5")
+
+h5.innerText = "Perfume Gucci by Bloom";
+
+const text = document.querySelector(".card-text")
+
+text.innerText = "Tonos suaves de lavanda";
+
