7e4e226c8018afce0b531cb8357b66bb40e354a5
Nahir <152107911+Nahir13@users.noreply.github.com>
2024-09-10 09:09:59 -0300

    add cards with javascript

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const h5 = document.querySelector("h5")
+const h1 = document.querySelector("h1");
+h1.innerText = "Productos";
 
-h5.innerText = "Perfume Gucci by Bloom";
+const container = document.querySelector("section");
+const cards = [];
 
-const text = document.querySelector(".card-text")
-
-text.innerText = "Tonos suaves de lavanda";
-
+ for (let i = 1; i < 4; i++) {
+    const card = `<div class="card">
+             <img src= "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" alt="Imagen ${i}">
+                     <h5>Autos</h5>
+                     <p>Texto</p>
+             </div>
+         </div>
+         `;
+         cards.push(card);
+        }
+     container.innerHTML = cards.toString();
